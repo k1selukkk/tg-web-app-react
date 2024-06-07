cf31c1699792f5f520722d9aa956998d72cffd57
Jast15 <125470080+Jast15@users.noreply.github.com>
2024-06-07 11:24:35 +0400

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+
+const Telegraf = require('telegraf');
+const bot = new Telegraf('6839956141:AAEHaAlbHk-m5VFPFkrJDXflekn5AHR6inM');
 
 function getClassByRate(vote) {
   if (vote >= 7) {
@@ -11,10 +15,30 @@ function getClassByRate(vote) {
 }
 
 function Movie({ movie, onClick }) {
-  const rating = movie.rating || movie.ratingKinopoisk || movie.ratingImdb; // Убедимся, что рейтинг отображается независимо от структуры данных
+  const rating = movie.rating || movie.ratingKinopoisk || movie.ratingImdb;
+
+  const handleClick = () => {
+    onClick();
+    sendMovieInfoToBot(movie);
+  };
+
+  const sendMovieInfoToBot = async (movie) => {
+    try {
+      await axios.post(`https://api.telegram.org/bot${bot.token}/sendMessage`, {
+        chat_id: '983259207',
+        text: `
+          Название: ${movie.nameRu || movie.nameEn}
+          Жанры: ${movie.genres.map(genre => genre.genre).join(', ')}
+          Рейтинг: ${rating}
+        `,
+      });
+    } catch (error) {
+      console.error('Ошибка отправки сообщения в Telegram:', error);
+    }
+  };
 
   return (
-    <div className="movie" onClick={onClick}>
+    <div className="movie" onClick={handleClick}>
       <div className="movie__cover-inner">
         <img src={movie.posterUrlPreview || movie.posterUrl} className="movie__cover" alt={movie.nameRu || movie.nameEn} />
         <div className="movie__cover--darkened"></div>
@@ -31,3 +55,4 @@ function Movie({ movie, onClick }) {
 }
 
 export default Movie;
+

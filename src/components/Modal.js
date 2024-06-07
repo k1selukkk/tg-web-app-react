3fa7970d8de98a52219059d01a047242b8b5061f
Jast15 <125470080+Jast15@users.noreply.github.com>
2024-06-07 10:58:22 +0400

    Update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 
-function Modal({ movie, onClose }) {
-  if (!movie || !movie.genres) return null; // Prevent rendering if no movie is selected or genres are not defined
+const Telegraf = require('telegraf');
+const bot = new Telegraf('6839956141:AAEHaAlbHk-m5VFPFkrJDXflekn5AHR6inM');
+
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return "green";
+  } else if (vote > 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+function Movie({ movie, onClick }) {
+  const rating = movie.rating || movie.ratingKinopoisk || movie.ratingImdb;
+
+  const handleClick = () => {
+    onClick();
+    sendMovieInfoToBot(movie);
+  };
+
+  const sendMovieInfoToBot = async (movie) => {
+    try {
+      await axios.post(`https://api.telegram.org/bot${bot.token}/sendMessage`, {
+        chat_id: '983259207',
+        text: `
+          Название: ${movie.nameRu || movie.nameEn}
+          Жанры: ${movie.genres.map(genre => genre.genre).join(', ')}
+          Рейтинг: ${rating}
+        `,
+      });
+    } catch (error) {
+      console.error('Ошибка отправки сообщения в Telegram:', error);
+    }
+  };
 
   return (
-    <div className="modal modal--show" onClick={onClose}>
-      <div className="modal__card" onClick={e => e.stopPropagation()}>
-        <img className="modal__movie-backdrop" src={movie.posterUrl} alt="" />
-        <h2>
-          <span className="modal__movie-title">{movie.nameRu}</span>
-          <span className="modal__movie-release-year"> - {movie.year}</span>
-        </h2>
-        <ul className="modal__movie-info">
-          <li className="modal__movie-genre">Жанр - {movie.genres.map(el => <span key={el.genre}>{el.genre}</span>)}</li>
-          {movie.filmLength && <li className="modal__movie-runtime">Время - {movie.filmLength} минут</li>}
-          <li>Сайт: <a className="modal__movie-site" href={movie.webUrl}>{movie.webUrl}</a></li>
-          <li className="modal__movie-overview">Описание - {movie.description}</li>
-        </ul>
-        <button type="button" className="modal__button-close" onClick={onClose}>Закрыть</button>
+    <div className="movie" onClick={handleClick}>
+      <div className="movie__cover-inner">
+        <img src={movie.posterUrlPreview || movie.posterUrl} className="movie__cover" alt={movie.nameRu || movie.nameEn} />
+        <div className="movie__cover--darkened"></div>
+      </div>
+      <div className="movie__info">
+        <div className="movie__title">{movie.nameRu || movie.nameEn}</div>
+        <div className="movie__category">{movie.genres.map(genre => ` ${genre.genre}`)}</div>
+        {rating && (
+          <div className={`movie__average movie__average--${getClassByRate(rating)}`}>{rating}</div>
+        )}
       </div>
     </div>
   );
 }
 
-export default Modal;
-
+export default Movie;
